3a438263dc548f59164147cc21803eebb3446575
Toto <elmundodetoto2006@gmail.com>
2025-04-13 18:25:39 -0300

    first commit

---
+++ Logica/Logica.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Referencias
+using Datos;
+using Servicios;
+using Sesion;
+
+namespace Logica
+{
+    internal class Logica
+    {
+    }
+}
