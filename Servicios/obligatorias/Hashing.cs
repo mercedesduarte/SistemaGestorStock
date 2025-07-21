640992fa210056942038dc37284fa3cc6c628c5f
iMagmel <novelinorodrigo3@gmail.com>
2025-07-21 12:48:21 -0300

    servicios obligatorios

---
+++ Servicios/obligatorias/Hashing.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Servicios.obligatorias
+{
+    internal class Hashing
+    {
+        public static string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
---
+++ Servicios/obligatorias/PrimerContra.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Servicios.obligatorias;
+
+namespace Servicios.obligatorias
+{
+    internal class PrimerContra
+    {
+        private static readonly string[] preguntasdeseguridad = new string[]
+        {
+            "¿Nombre de tu primera mascota?",
+            "¿Ciudad donde naciste?",
+            "¿Nombre de tu escuela primaria?",
+            "¿Nombre de tu mejor amigo de la infancia?"
+        };
+
+        public static string Generarcontrarandom(int length = 10)
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*";
+            StringBuilder password = new StringBuilder();
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                byte[] buffer = new byte[1];
+                for (int i = 0; i < length; i++)
+                {
+                    rng.GetBytes(buffer);
+                    int index = buffer[0] % chars.Length;
+                    password.Append(chars[index]);
+                }
+            }
+            return password.ToString();
+        }
+
+        public static List<string> Mostrarpreguntas()
+        {
+            return new List<string>(preguntasdeseguridad);
+        }
+
+        public class NuevoUsuario
+        {
+            public string Username { get; set; }
+            public string HashedPassword { get; set; }
+            public bool DebeCambiarContraseña { get; set; } = true;
+            public Dictionary<string, string> PreguntasSeguridad { get; set; }
+        }
+
+        // Crear usuario con contraseña aleatoria y preguntas de seguridad
+        public static NuevoUsuario CrearNuevoUsuario(string username, Dictionary<string, string> respuestas)
+        {
+            string passwordGenerada = Generarcontrarandom();
+            string hash = HashconUsu.HashWithUsername(username, passwordGenerada);
+
+            return new NuevoUsuario
+            {
+                Username = username,
+                HashedPassword = hash,
+                DebeCambiarContraseña = true,
+                PreguntasSeguridad = respuestas
+            };
+        }
+    }
+}
