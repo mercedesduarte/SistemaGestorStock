46d13a00728ceb8b90360884b09bd3870751d495
Sencriox Yt <elmundodetoto2006@gmail.com>
2025-07-25 18:10:38 -0300

    FIX: Gitignore change

---
+++ Servicios/CantidadPreguntas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Servicios
+{
+    public class CantidadPreguntas
+    {
+
+        // Valida que la cantidad de preguntas sea mayor igual a la cantidad de preguntas que asigno el admin en la tabla Restricciones
+        public static void ValidarCantidadPreguntas(ObjetoPreguntas obj, int respuestas)
+        {
+            if (obj.preguntas != respuestas)
+            {
+                throw new ArgumentException("Completa todas las preguntas");
+            }
+
+        }
+    }
+}
---
+++ Servicios/MinimoContra.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    public class MinimoContra
+    {
+        // Debemos extraer del DTO de la capa logica el número de caracteres que debe tener la contraseña, EN LA TABLA Restricciones 
+        public static void ValidarMinimoCaracteres(string password, int minimoContra)
+        {
+            if (password.Length < minimoContra)
+            {
+                throw new Exception("La contraseña debe tener al menos" + " caracteres");
+            }
+        }
+    }
+}
