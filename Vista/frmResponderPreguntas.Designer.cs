10143934458f50e319cf075a27da06f89d0380f0
Geronimo Carpignano <gerofoxy@gmail.com>
2025-07-30 11:32:57 -0300

    Cree el coso para responder las preguntas

---
+++ Vista/frmResponderPreguntas.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Vista
+{
+    partial class frmResponderPreguntas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmResponderPreguntas));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnCrearPregunta = new System.Windows.Forms.Button();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.txtPregunta = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPregunta = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox1.Controls.Add(this.lblPregunta);
+            this.groupBox1.Controls.Add(this.btnCerrar);
+            this.groupBox1.Controls.Add(this.btnCrearPregunta);
+            this.groupBox1.Controls.Add(this.lblUsuario);
+            this.groupBox1.Controls.Add(this.txtPregunta);
+            this.groupBox1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(-1, 101);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(823, 348);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Responde las preguntas";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnCerrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCerrar.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnCerrar.Image = ((System.Drawing.Image)(resources.GetObject("btnCerrar.Image")));
+            this.btnCerrar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCerrar.Location = new System.Drawing.Point(506, 235);
+            this.btnCerrar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Padding = new System.Windows.Forms.Padding(13, 0, 13, 0);
+            this.btnCerrar.Size = new System.Drawing.Size(185, 55);
+            this.btnCerrar.TabIndex = 14;
+            this.btnCerrar.Text = "CERRAR";
+            this.btnCerrar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCerrar.UseVisualStyleBackColor = false;
+            // 
+            // btnCrearPregunta
+            // 
+            this.btnCrearPregunta.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnCrearPregunta.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCrearPregunta.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnCrearPregunta.Image = ((System.Drawing.Image)(resources.GetObject("btnCrearPregunta.Image")));
+            this.btnCrearPregunta.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCrearPregunta.Location = new System.Drawing.Point(120, 235);
+            this.btnCrearPregunta.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCrearPregunta.Name = "btnCrearPregunta";
+            this.btnCrearPregunta.Padding = new System.Windows.Forms.Padding(13, 0, 13, 0);
+            this.btnCrearPregunta.Size = new System.Drawing.Size(336, 55);
+            this.btnCrearPregunta.TabIndex = 13;
+            this.btnCrearPregunta.Text = "RESPONDER PREGUNTA";
+            this.btnCrearPregunta.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCrearPregunta.UseVisualStyleBackColor = false;
+            this.btnCrearPregunta.Click += new System.EventHandler(this.btnCrearPregunta_Click);
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(115, 86);
+            this.lblUsuario.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(149, 32);
+            this.lblUsuario.TabIndex = 11;
+            this.lblUsuario.Text = "PREGUNTA:";
+            // 
+            // txtPregunta
+            // 
+            this.txtPregunta.Location = new System.Drawing.Point(120, 154);
+            this.txtPregunta.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPregunta.Name = "txtPregunta";
+            this.txtPregunta.Size = new System.Drawing.Size(571, 38);
+            this.txtPregunta.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.label1.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(-1, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(823, 66);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "CREAR PREGUNTAS DE SEGURIDAD";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPregunta
+            // 
+            this.lblPregunta.AutoSize = true;
+            this.lblPregunta.Location = new System.Drawing.Point(271, 86);
+            this.lblPregunta.Name = "lblPregunta";
+            this.lblPregunta.Size = new System.Drawing.Size(134, 32);
+            this.lblPregunta.TabIndex = 15;
+            this.lblPregunta.Text = "Responde";
+            // 
+            // frmResponderPreguntas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 455);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "frmResponderPreguntas";
+            this.Text = "frmResponderPreguntas";
+            this.Load += new System.EventHandler(this.frmResponderPreguntas_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnCrearPregunta;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.TextBox txtPregunta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPregunta;
+    }
+}
