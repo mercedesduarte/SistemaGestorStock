f79d8473e3dc501997f3edbff2fd2864ee6c9daf
iMagmel <novelinorodrigo3@gmail.com>
2025-08-03 14:22:37 -0300

    clase log en capa sesion

---
+++ Sesion/Log.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion
+{
+    internal class Log
+    {
+        public int IdLog { get; set; }
+        public int IdUsuario { get; set; }
+        public string Usuario { get; set; }
+        public string Accion { get; set; }
+
+        public Log(int idLog, int idUsuario, string usuario, string accion)
+        {
+            IdLog = idLog;
+            IdUsuario = idUsuario;
+            Usuario = usuario;
+            Accion = accion;
+        }
+
+        public string ObtenerMensaje()
+        {
+            if (IdUsuario > 0)
+                return $"El usuario {Usuario} realizó la acción: {Accion}";
+            else
+                throw new Exception("El ID del usuario no puede ser encontrado");
+        }
+    }
+}
